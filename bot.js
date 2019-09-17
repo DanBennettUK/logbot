2d4d6f8836ac201db1e50cd0322f24ad2812a9f1
Your Name <sergejmatek@gmail.com>
2019-09-17 18:05:28 +0200

    Logging update

--- bot.js
+++ bot.js
@@ -4442,6 +4442,150 @@ client.on('guildUpdate', function (oldGuild, newGuild) {
     }
 });
 
+client.on('channelCreate', function (channel) {
+    if (modulesFile.get('EVENT_CHANNEL_CREATE_LOG')) {
+        if (![`dm`, `group`].includes(channel.type)) {
+            var channelType = 'Channel'
+            if (channel.type == `text`) {
+                channelType = `Text channel`;
+            } else if (channel.type == `voice`) {
+                channelType = `Voice channel`;
+            } else if (channel.type == `category`) {
+                channelType = `Category`;
+            } else if (channel.type == `news`) {
+                channelType = `News channel`;
+            } else if (channel.type == `store`) {
+                channelType = `Store channel`;
+            }
+            channel.guild.channels.get(config.channel_serverlog).send({
+                embed: {
+                    color: config.color_success,
+                    author: {
+                        name: channel.guild.name,
+                        icon_url: channel.guild.iconURL
+                    },
+                    title: `${channelType} creation`,
+                    description: `${channelType} ${channel} has been created`,
+                    fields: [
+                        {
+                            name: 'Name',
+                            value: `${channel.name}`,
+                            inline: true
+                        },
+                        {
+                            name: 'ID',
+                            value: `${channel.id}`,
+                            inline: true
+                        }
+                    ],
+                    timestamp: new Date(),
+                    footer: {
+                        text: `Marvin's Little Brother | Current version: ${config.version}`
+                    }
+                }
+            });
+        }
+    }
+});
+
+client.on('channelDelete', function (channel) {
+    if (modulesFile.get('EVENT_CHANNEL_DELETE_LOG')) {
+        if (![`dm`, `group`].includes(channel.type)) {
+            var channelType = 'Channel'
+            if (channel.type == `text`) {
+                channelType = `Text channel`;
+            } else if (channel.type == `voice`) {
+                channelType = `Voice channel`;
+            } else if (channel.type == `category`) {
+                channelType = `Category`;
+            } else if (channel.type == `news`) {
+                channelType = `News channel`;
+            } else if (channel.type == `store`) {
+                channelType = `Store channel`;
+            }
+            channel.guild.channels.get(config.channel_serverlog).send({
+                embed: {
+                    color: config.color_warning,
+                    author: {
+                        name: channel.guild.name,
+                        icon_url: channel.guild.iconURL
+                    },
+                    title: `${channelType} deletion`,
+                    description: `${channelType} ${channel} has been deleted`,
+                    fields: [
+                        {
+                            name: 'Name',
+                            value: `${channel.name}`,
+                            inline: true
+                        },
+                        {
+                            name: 'ID',
+                            value: `${channel.id}`,
+                            inline: true
+                        }
+                    ],
+                    timestamp: new Date(),
+                    footer: {
+                        text: `Marvin's Little Brother | Current version: ${config.version}`
+                    }
+                }
+            });
+        }
+    }
+});
+
+client.on('channelUpdate', function (oldChannel, newChannel) {
+    if (modulesFile.get('EVENT_CHANNEL_UPDATE_LOG')) {
+        if (![`dm`, `group`].includes(oldChannel.type)) {
+            var channelType = 'Channel'
+            if (oldChannel.type == `text`) {
+                channelType = `Text channel`;
+            } else if (oldChannel.type == `voice`) {
+                channelType = `Voice channel`;
+            } else if (oldChannel.type == `category`) {
+                channelType = `Category`;
+            } else if (oldChannel.type == `news`) {
+                channelType = `News channel`;
+            } else if (oldChannel.type == `store`) {
+                channelType = `Store channel`;
+            }
+            if (oldChannel.name !== newChannel.name) {
+                oldChannel.guild.channels.get(config.channel_serverlog).send({
+                    embed: {
+                        color: config.color_info,
+                        author: {
+                            name: oldChannel.guild.name,
+                            icon_url: oldChannel.guild.iconURL
+                        },
+                        title: `${channelType} name change`,
+                        description: `Name of ${channelType.toLowerCase()} ${newChannel} has been changed`,
+                        fields: [
+                            {
+                                name: 'Old name',
+                                value: `${oldChannel.name}`,
+                                inline: true
+                            },
+                            {
+                                name: 'New name',
+                                value: `${newChannel.name}`,
+                                inline: true
+                            },
+                            {
+                                name: 'ID',
+                                value: `${oldChannel.id}`
+                            }
+                        ],
+                        timestamp: new Date(),
+                        footer: {
+                            text: `Marvin's Little Brother | Current version: ${config.version}`
+                        }
+                    }
+                });
+            }
+        }
+    }
+});
+
 client.on('messageReactionAdd', function (messageReaction, user) {
     if (modulesFile.get('EVENT_MESSAGE_REACTION_ADD')) {
         switch (messageReaction.emoji.name) {
