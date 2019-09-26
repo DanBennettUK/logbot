eebcca73c1c34cd33d6201031d062be9ace704c5
Your Name <sergejmatek@gmail.com>
2019-09-26 21:50:06 +0200

    Properly display amount of commands listed

--- bot.js
+++ bot.js
@@ -2665,13 +2665,15 @@ client.on('message', async message => {
             }
             if (args[0].toLowerCase() === 'list') {
                 var cKeys = _.keys(customCommands.read());
+                var numberOfCommands = 0;
                 var allCommands = '';
+                var plural = 'commands';
                 for (var i = 0; i < cKeys.length; i++) {
                     if (allCommands.length > 1900) {
                         message.channel.send({
                             embed: {
                                 color: config.color_info,
-                                title: `**Listing ${i + 1} custom commands:**`,
+                                title: `**Listing ${numberOfCommands} custom ${plural}:**`,
                                 author: {
                                     name: client.user.username,
                                     icon_url: client.user.displayAvatarURL
@@ -2684,24 +2686,31 @@ client.on('message', async message => {
                             }
                         });
                         allCommands = ``;
+                        numberOfCommands = 0;
                     }
+                    numberOfCommands++;
                     allCommands +=`\n **${cKeys[i]}:** ${customCommands.get(cKeys[i]).content}`;
-                }
-                message.channel.send({
-                    embed: {
-                        color: config.color_info,
-                        title: `**Listing all custom commands:**`,
-                        author: {
-                            name: client.user.username,
-                            icon_url: client.user.displayAvatarURL
-                        },
-                        description: `${allCommands}`,
-                        timestamp: new Date(),
-                        footer: {
-                            text: `Marvin's Little Brother | Current version: ${config.version}`
-                        }
+                    if (numberOfCommands == 1) {
+                        plural = 'command';
+                    } else plural = 'commands';
+                    if (i === (cKeys.length - 1)) {
+                        message.channel.send({
+                            embed: {
+                                color: config.color_info,
+                                title: `**Listing ${numberOfCommands} custom ${plural}:**`,
+                                author: {
+                                    name: client.user.username,
+                                    icon_url: client.user.displayAvatarURL
+                                },
+                                description: `${allCommands}`,
+                                timestamp: new Date(),
+                                footer: {
+                                    text: `Marvin's Little Brother | Current version: ${config.version}`
+                                }
+                            }
+                        });
                     }
-                });
+                }
             }
         }
     }
