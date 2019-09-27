40d0396bf2af7e2375cedf8919231aa7d278c819
Your Name <sergejmatek@gmail.com>
2019-09-27 09:37:00 +0200

    Improved parseChannelTag

--- bot.js
+++ bot.js
@@ -435,6 +435,21 @@ function parseChannelTag(tag) {
         return trimMe.replace(/[^0-9.]/gi, '');
     } else if (/^[0-9]+$/.test(tag)) {
         return trimMe;
+    } else if (/#.+/.test(tag)) {
+        trimMe.replace('#', '');
+        var chnl = guild.channels.find(c => c.name === trimMe);
+        if (chnl == null) {
+            return 'err';
+        } else {
+            return chnl.id;
+        }
+    } else if (/.+/.test(tag)) {
+        var chnl = guild.channels.find(c => c.name === trimMe);
+        if (chnl == null) {
+            return 'err';
+        } else {
+            return chnl.id;
+        }
     } else {
         return 'err';
     }
