c3de583fa69f99d93be87401c89fd73a6ff40c62
Your Name <sergejmatek@gmail.com>
2019-10-28 02:22:57 +0100

    Multiple image sources

--- commands/cat.js
+++ commands/cat.js
@@ -3,11 +3,23 @@ exports.run = (client, message, args) => {
     const request = client.request;
     if (message.member.roles.some(role => ['Moderators', 'Support'].includes(role.name))) {
         if (modulesFile.get('COMMAND_ANIMAL_PICTURE')) {
-            request(`https://some-random-api.ml/img/cat`,
-            function (error, response, body) {
-                answer = JSON.parse(body);
-                message.channel.send(answer['link']);
-            });            
+            var random = Math.floor(Math.random() * Math.floor(2));
+            switch(random) {
+                case 0:
+                    request(`https://some-random-api.ml/img/cat`,
+                    function (error, response, body) {
+                        answer = JSON.parse(body);
+                        message.channel.send(answer['link']);
+                    });
+                    break;
+                case 1:
+                    request(`http://aws.random.cat/meow`,
+                    function (error, response, body) {
+                        answer = JSON.parse(body);
+                        message.channel.send(answer['file']);
+                    });
+                    break;
+            }        
         }
     } // End of permission checking statement
 }
--- commands/dog.js
+++ commands/dog.js
@@ -3,11 +3,23 @@ exports.run = (client, message, args) => {
     const request = client.request;
     if (message.member.roles.some(role => ['Moderators', 'Support'].includes(role.name))) {
         if (modulesFile.get('COMMAND_ANIMAL_PICTURE')) {
-            request(`https://some-random-api.ml/img/dog`,
-            function (error, response, body) {
-                answer = JSON.parse(body);
-                message.channel.send(answer['link']);
-            });            
+            var random = Math.floor(Math.random() * Math.floor(2));
+            switch(random) {
+                case 0:
+                    request(`https://some-random-api.ml/img/dog`,
+                    function (error, response, body) {
+                        answer = JSON.parse(body);
+                        message.channel.send(answer['link']);
+                    }); 
+                    break;
+                case 1:
+                    request(`https://dog.ceo/api/breeds/image/random`,
+                    function (error, response, body) {
+                        answer = JSON.parse(body);
+                        message.channel.send(answer['message']);
+                    });
+                    break;
+            }         
         }
     } // End of permission checking statement
 }
