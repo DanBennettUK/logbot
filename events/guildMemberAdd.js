2657bf7eb4025e50de5342ef82770c1910eb5dd2
Your Name <sergejmatek@gmail.com>
2019-11-02 22:33:29 +0100

    Improved readability of bannned user system notes

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -93,7 +93,7 @@ module.exports = (client, member) => {
                     }).catch(console.error);
                 }
                 var identifier = cryptoRandomString({length: 10});
-                connection.query('INSERT INTO log_note (userID, actioner, description, identifier, isDeleted, timestamp) VALUES (?,?,?,?,?,?)', [member.id, '001', msg.join(' '), identifier, 0, new Date()],
+                connection.query('INSERT INTO log_note (userID, actioner, description, identifier, isDeleted, timestamp) VALUES (?,?,?,?,?,?)', [member.id, '001', msg.join(' ').replace(/`/g, ''), identifier, 0, new Date()],
                 function(err, results){
                     if (err) throw err;
                 });
