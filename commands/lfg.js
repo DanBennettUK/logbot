e0a4150c22017fadc95f4e87d027ca57451a7e41
Your Name <sergejmatek@gmail.com>
2019-12-06 21:25:01 +0100

    Proper LFG fix

--- commands/lfg.js
+++ commands/lfg.js
@@ -3,9 +3,11 @@ exports.run = (client, message, args) => {
     if (message.member.roles.some(role => 'Moderators' == role.name)) {
         if (modulesFile.get('COMMAND_LFG')) {
             message.delete().then(() => {
-                message.channel.messages.first().delete().then(m => {
-                    message.channel.send(`${m.author}, please use the appropriate channel for LFG requests, not ${m.channel} `).then(msg => {
-                        setTimeout(() => msg.delete(), 7000);
+                message.channel.fetchMessages({limit: 1}).then(msgs => {
+                    msgs.first().delete().then(m => {
+                        message.channel.send(`${m.author}, please use the appropriate channel for LFG requests, not ${m.channel} `).then(msg => {
+                            setTimeout(() => msg.delete(), 7000);
+                        }).catch(console.error);
                     }).catch(console.error);
                 }).catch(console.error);
             }).catch(console.error);
