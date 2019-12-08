143d072a0ae86f2446f011f1ede9b63b486fded3
Your Name <sergejmatek@gmail.com>
2019-12-09 00:19:20 +0100

    Added 'panda' command

---
+++ commands/panda.js
@@ -0,0 +1,35 @@
+exports.run = (client, message, args) => {
+    const modulesFile = client.modulesFile;
+    const request = client.request;
+    if (message.member.roles.some(role => ['Moderators', 'Support'].includes(role.name))) {
+        if (modulesFile.get('COMMAND_ANIMAL_PICTURE')) {
+            var random = Math.floor(Math.random() * Math.floor(2));
+            switch (random) {
+                case 0:
+                    request(`https://some-random-api.ml/img/panda`,
+                    function (error, response, body) {
+                        answer = JSON.parse(body);
+                        const attachment = new client.Discord.Attachment(answer['link']);
+                        message.channel.send('Fetching picture...').then(msg => {
+                            message.channel.send(attachment).then(() => {
+                                msg.delete().catch(console.error);
+                            }).catch(console.error);
+                        }).catch(console.error);
+                    });
+                    break;
+                case 1:
+                    request(`https://some-random-api.ml/img/red_panda`,
+                    function (error, response, body) {
+                        answer = JSON.parse(body);
+                        const attachment = new client.Discord.Attachment(answer['link']);
+                        message.channel.send('Fetching picture...').then(msg => {
+                            message.channel.send(attachment).then(() => {
+                                msg.delete().catch(console.error);
+                            }).catch(console.error);
+                        }).catch(console.error);
+                    });
+                    break;
+            }
+        }
+    } // End of permission checking statement
+}
