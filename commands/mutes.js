3d07c88d43befbedb7ceb727b95dc1e62c005b83
Your Name <sergejmatek@gmail.com>
2020-01-04 22:32:38 +0100

    Added 'remaining' field to 'mutes list'

--- commands/mutes.js
+++ commands/mutes.js
@@ -10,10 +10,49 @@ exports.run = (client, message, args) => {
                         let user = client.users.get(key);
                         const start = new Date(mutes[key].start * 1000).toUTCString();
                         const end = new Date(mutes[key].end * 1000).toUTCString();
+                        let s = Math.floor(mutes[key].end - (Date.now() / 1000));
+                        let d = 0;
+                        let h = 0;
+                        let m = 0;
+                        while (s >= 60) {
+                            while (m >= 60) {
+                                while (h >= 24) {
+                                    h -= 24;
+                                    d++;
+                                }
+                                m -= 60;
+                                h++;
+                            }
+                            s -= 60;
+                            m++;
+                        }
+                        let time = '';
+                        switch (d) {
+                            case 0:
+                                switch (h) {
+                                    case 0:
+                                        switch (m) {
+                                            case 0:
+                                                time = `${s}s`;
+                                                break;
+                                            default:
+                                                time = `${m}m${s}s`
+                                                break;
+                                        }
+                                        break;
+                                    default:
+                                        time = `${h}h${m}m${s}s`
+                                        break;
+                                }
+                                break;
+                            default:
+                                time = `${d}d${h}h${m}m${s}s`
+                                break;
+                        }
                         if (!user) user = client.fetchUser(key);
                         fields.push({
                             name: key,
-                            value: `**User:** ${user}\n**Identifier:** ${mutes[key].identifier}\n**Reason:** ${mutes[key].reason}\n**Start:** ${start}\n**End:** ${end}\n**Duration:** ${mutes[key].duration}`
+                            value: `**User:** ${user}\n**Identifier:** ${mutes[key].identifier}\n**Reason:** ${mutes[key].reason}\n**Start:** ${start}\n**End:** ${end}\n**Duration:** ${mutes[key].duration}\n**Remaining:** ${time}`
                         });
                     }
                     if (fields.length == 0) message.channel.send('No mutes are set.').catch(console.error);
